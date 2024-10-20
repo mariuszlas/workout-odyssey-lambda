e716a2458d6097a97bbafa97c38a3d21dbd6afc6
Mariusz Las <mariusz.las@icloud.com>
2024-10-20 14:46:02 +0100

    use terraform to make zip  file for lambda

--- backend.tf
+++ backend.tf
@@ -1,8 +1,15 @@
 terraform {
+  required_version = ">= 1.9.6"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~>5.43"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.6.0"
     }
   }
 
